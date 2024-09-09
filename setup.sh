d0613750fc5fdeaa77b94009b94b16722472a170
cairndean <163149358+cairndean@users.noreply.github.com>
2024-09-09 17:55:19 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+pip install Flask==2.1.1
+pip install Flask-SQLAlchemy==2.5.1
+pip install SQLAlchemy==1.4.32
+pip install gunicorn
+pip install werkzeug==0.16.1
+pip install python-dotenv
+pip install psycopg2-binary
